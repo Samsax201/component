f3070ba83e31f0a4ddf1fd3e8f5662bff956e653
Samsax201 <oluwasanmisamuel001@gmail.com>
2022-08-08 13:43:26 +0100

    javascript na die

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('orangediv').style.background = "black"
+
+//es 5
+
+var seconddiv = document.getElementById("orangediv")
+
+seconddiv.addEventListener('click', function (){
+        alert('orangediv is clicked')
+})
